74605c584b78f4566764c6e706ce400833b8d5cc
Pedro Araujo <pedro@pedroaraujo.net>
2020-11-19 00:38:31 +0000

    Replace php echo with short open tags

--- src/index.php
+++ src/index.php
@@ -3,19 +3,21 @@
 
 // include blog info
 include("info.php");
+
 ?>
 <!DOCTYPE html>
-<html lang="<?php echo $lang; ?>">
+<html lang="<?=$lang?>">
 <head>
-<?php echo "$head_content\n"; ?>
-<title><?php echo $blog_title; ?></title>
+<meta charset="utf-8">
+<?=$head_content?>
+<title><?=$blog_title?></title>
 </head>
 <body>
 <header>
-<h1><?php echo $blog_title; ?></h1>
+<h1><?=$blog_title?></h1>
 <div id="blog-info">
-<?php echo "$blog_info\n"; ?>
-<p>[[ <a href="toc.php"><?php echo "$toc_link"; ?></a> ]]</p>
+<?=$blog_info?>
+<p>[[ <a href="toc.php"><?=$toc_link?></a> ]]</p>
 </div>
 </header>
 <main>
@@ -25,7 +27,7 @@ include("info.php");
 $files = array();
 $dir = opendir('source');
 while(false != ($file = readdir($dir))) {
-	if(($file != ".") and ($file != "..") and ($file != "index.php")) {
+	if(($file != ".") and ($file != "..")) {
 		$files[] = $file;
 	}
 }
@@ -60,23 +62,27 @@ foreach ($files as $file) {
 		$file_date = substr($file_date, 0, -9);
 
 		// print file content to html
-		echo "<!-- ARTICLE -->\n";
-		echo "<div class='article' id='$filename'>\n";
-		echo "<span class='entry-url'>$published: $file_date</span>\n";
-		echo "<div class='content'>\n";
-		echo "<a class='standalone-link' href='article.php?id=$filename'>&#128279;</a>\n";
-		echo file_get_contents("source/$file");
-		echo "</div><!--content-->\n";
-		echo "</div><!--$filename-->\n\n";
+?>
+
+<!-- ARTICLE -->
+<div class="article" id="<?=$filename?>">
+<span class="entry-url"><?=$published?>: <?=$file_date?></span>
+<div class="content">
+<a class="standalone-link" href="article.php?id=<?=$filename?>">&#128279;</a>
+<?php echo file_get_contents("source/$file"); ?>
+</div><!--content-->
+</div><!--$filename-->
+
+<?php
 	}
 }
 
 // if there are more than 10 articles, show the next message:
-if ($max_articles > $max_number_articles) {
-	echo "<p id='toc_message'>[[ <a href='toc.php'>$toc_message</a>. ]]</p>";
-}
+if ($max_articles > $max_number_articles)
+{ ?>
+<p id="toc_message">[[ <a href="toc.php"><?=$toc_message?></a>. ]]</p>
+<?php } ?>
 
-?>
 </main>
 </body>
 </html>
